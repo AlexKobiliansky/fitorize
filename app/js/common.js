50c6ad84be75a674ceb32f747a0b8f2050b3ef0c
alex kobilianskiy <alex_kobilianskiy@bigmir.net>
2018-11-03 00:33:16 +0200

    section products complete

--- app/js/common.js
+++ app/js/common.js
@@ -10,6 +10,9 @@ $(document).ready(function() {
     function heightses() {
         if ($(window).width()>=481) {
             $(".adv-item-desc").height('auto').equalHeights();
+            $('.prod-item-name').matchHeight();
+            $('.prod-item-price').matchHeight();
+            $('.prod-item').matchHeight();
         }
     }
 
@@ -19,6 +22,26 @@ $(document).ready(function() {
 
     heightses();
 
+
+
+
+    $('.prod-slider').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:true,
+        navText: ["", ""],
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    })
 
     //E-mail Ajax Send
     $("form").submit(function() { //Change
