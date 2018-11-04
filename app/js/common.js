79bdea2e3e0386b2a664352dfd101e944fb18c52
alex kobilianskiy <alex_kobilianskiy@bigmir.net>
2018-11-04 06:54:02 +0200

    functionality complete

--- app/js/common.js
+++ app/js/common.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
 
+    $('.preloader').fadeOut();
+
     $(".toggle-mnu").click(function() {
         $(this).toggleClass("on");
         $(".mobile-mnu").slideToggle();
         return false;
     });
 
+    $(".main-mnu a").mPageScroll2id();
+
+    $('.mobile-mnu a').on('click', function(){
+       $(this).parents('.mobile-mnu').slideToggle();
+       $('.toggle-mnu').toggleClass('on');
+    }).mPageScroll2id();
 
     function heightses() {
         if ($(window).width()>=481) {
@@ -31,6 +39,35 @@ $(document).ready(function() {
 
     heightses();
 
+    /**
+     * toTop functionality start
+     */
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 1000) {
+            $('#toTop1').css('opacity', '.6');
+        } else {
+            $('#toTop1').css('opacity', '0');
+        }
+    });
+
+    $('body').bind('touchmove', function (e)
+    {
+        if($(this).scrollTop() > 1000) {
+            $('#toTop1').css('opacity', '.6');
+        } else {
+            $('#toTop1').css('opacity', '0');
+        }
+    });
+
+    $('#toTop1').click(function() {
+        $('body,html').animate({scrollTop:0},600);
+    });
+    /**
+     * toTop functionality end
+     */
+
+
+
     $('.prod-slider').owlCarousel({
         loop:true,
         margin:30,
@@ -55,6 +92,61 @@ $(document).ready(function() {
     $.validate({
         form : '.contact-form',
     });
+
+    $(function() {
+        $("a[href='#popup-form']").magnificPopup({
+            type: "inline",
+            fixedContentPos: !1,
+            fixedBgPos: !0,
+            overflowY: "auto",
+            closeBtnInside: !0,
+            preloader: !1,
+            midClick: !0,
+            removalDelay: 300,
+            mainClass: "my-mfp-zoom-in"
+        })
+    });
+
+
+    /**
+     * animations start
+     */
+
+
+    function animateTypicItems($item, $firstDelay, $timeoutDelay){
+        $item.each(function(){
+            $(this).attr("data-wow-delay", $firstDelay + "s");
+            if ($(window).width()>=991) {
+                $firstDelay += $timeoutDelay;
+            }
+        });
+    }
+
+    animateTypicItems($('.adv-item'), .7, .3);
+    animateTypicItems($('.income-item'), .5, .3);
+    animateTypicItems($('.bonus-item li'), .5, .3);
+    animateTypicItems($('.wow-price-item'), .5, .3);
+
+    function animateAbout(){
+        $('.about-item:first').addClass('fadeInLeft');
+        $('.about-item:last').addClass('fadeInRight');
+    }
+
+    function animatePriceImg(){
+        var $t = 1;
+        $('.price-item').each(function(){
+            $(this).find('.price-item-img').attr("data-wow-delay", $t + "s");
+            if ($(window).width()>=991) {
+                $t += .3
+            }
+        });
+    }
+
+    animatePriceImg();
+    animateAbout();
+    /**
+     * animations end
+     */
 
     //E-mail Ajax Send
     $("form").submit(function() { //Change
